7edf9cae7c4c82d9a25092a8de49c00e02832db1
thomazcapra <thomaz.capra@gmail.com>
2018-04-19 19:50:43 -0300

    RF3 - Added more information about present and future

--- src/app/components/future/future.component.ts
+++ src/app/components/future/future.component.ts
@@ -1,15 +1,32 @@
+import { CardInfoModel } from './../../model/card-info.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-future',
-  templateUrl: './future.component.html',
-  styleUrls: ['./future.component.scss']
+    selector: 'app-future',
+    templateUrl: './future.component.html',
+    styleUrls: ['./future.component.scss']
 })
 export class FutureComponent implements OnInit {
 
-  constructor() { }
+    futureData = <Array<CardInfoModel>>[
+        new CardInfoModel(
+            'Full Stack Developer / Front End Developer',
+            `../assets/image/bold-icon.png`,
+            'Bold International',
+            'Almost there :)'
+        ),
+        new CardInfoModel(
+            'Master Degree - Computer Science',
+            `https://colegiomente-cerebro.ulisboa.pt/wp-content/uploads/2015/11/ULisboa_logo.png`,
+            'Universidade de Lisboa',
+            'Maybe cyber security'
+        )
+    ];
 
-  ngOnInit() {
-  }
+
+    constructor() { }
+
+    ngOnInit() {
+    }
 
 }
--- src/app/components/present/present.component.ts
+++ src/app/components/present/present.component.ts
@@ -1,15 +1,23 @@
+import { CardInfoModel } from './../../model/card-info.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-present',
-  templateUrl: './present.component.html',
-  styleUrls: ['./present.component.scss']
+    selector: 'app-present',
+    templateUrl: './present.component.html',
+    styleUrls: ['./present.component.scss']
 })
 export class PresentComponent implements OnInit {
 
-  constructor() { }
+    presentData = <CardInfoModel>
+        new CardInfoModel(
+            'Software Developer Analyst 4',
+            `https://www.opus-software.com.br/wp-content/uploads/2017/06/logotipo-opus-software.png`,
+            'OPUS SOFTWARE',
+            '2 main projects right here'
+        );
 
-  ngOnInit() {
-  }
+    constructor() { }
 
+    ngOnInit() {
+    }
 }
