17b4dfadfdc616db0499da5fd2ca6fbb32de9c74
thomaz.capra <thomaz.capra@gmail.com>
2019-01-16 23:35:26 +0000

    Updated to Angular 7

--- src/app/animations/logo.animation.ts
+++ src/app/animations/logo.animation.ts
@@ -1,38 +1,31 @@
 import {
-    AnimationEntryMetadata,
-    trigger,
-    style,
-    animate,
-    transition,
-    state
-} from '@angular/core';
+  trigger,
+  style,
+  animate,
+  transition,
+  state,
+  AnimationTriggerMetadata
+} from '@angular/animations';
 
 export const animationStateList: string[] = ['shown', 'scaleIn', 'scaleOut'];
 
-export const logoAnimation: AnimationEntryMetadata = trigger('logoAnimation', [
+export const logoAnimation: AnimationTriggerMetadata = trigger(
+  'logoAnimation',
+  [
     state('void', style({ opacity: 0 })),
     state('shown', style({ opacity: 1 })),
     state('scaleIn', style({ transform: 'scale(1.2)' })),
     state('scaleOut', style({ transform: 'scale(1)' })),
 
-    transition('* => shown',
-        [
-            animate('200ms ease-in', style({ opacity: 1 }))
-        ]
-    ),
-    transition('shown => scaleIn',
-        [
-            animate('500ms ease-in', style({ transform: 'scale(1.2)' }))
-        ]
-    ),
-    transition('scaleIn => scaleOut',
-        [
-            animate('500ms ease-out', style({ transform: 'scale(1)' }))
-        ]
-    ),
-    transition('* => hidden',
-        [
-            animate('200ms ease-out', style({ opacity: 0 }))
-        ]
-    )
-]);
+    transition('* => shown', [animate('200ms ease-in', style({ opacity: 1 }))]),
+    transition('shown => scaleIn', [
+      animate('500ms ease-in', style({ transform: 'scale(1.2)' }))
+    ]),
+    transition('scaleIn => scaleOut', [
+      animate('500ms ease-out', style({ transform: 'scale(1)' }))
+    ]),
+    transition('* => hidden', [
+      animate('200ms ease-out', style({ opacity: 0 }))
+    ])
+  ]
+);
